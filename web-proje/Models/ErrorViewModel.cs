d6b781cefe93d28b6adcb872435a6a863a258397
aleyna <aleynaebrt@gmail.com>
2021-12-14 16:56:18 +0300

    Test

--- web-proje/Models/ErrorViewModel.cs
+++ web-proje/Models/ErrorViewModel.cs
@@ -8,4 +8,11 @@ namespace web_proje.Models
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
+
+    public class ErrorViewModel2
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
